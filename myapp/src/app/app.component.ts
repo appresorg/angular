563461b7206cd3fc7cd866017f2b0ac5e71a1b27
APPRES <support@appres.org>
2021-03-10 15:35:06 +0900

    My App

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+
+declare var $$, $S, $T, $Q: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent implements OnInit {
+  title = '';
+
+  ngOnInit(): void {
+    let self = this;
+    window.onload = function(e) {
+      $$().ready(()=>{
+        self.title = $$("myapp");
+      });
+    }
+  }
 }
