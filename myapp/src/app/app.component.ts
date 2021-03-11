55d4fdb3d0d8bf30f210431ef02055702bff6bfc
APPRES <support@appres.org>
2021-03-11 14:58:25 +0900

    events

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 declare var $$, $S, $T, $Q: any;
-declare var $appresBinding$: any;
 
 @Component({
   selector: 'app-root',
@@ -16,29 +15,23 @@ export class AppComponent implements OnInit, OnDestroy {
   onReady = null;
   onLanguageChange = null;
 
-  onAppRes(): void {
+  onAppResTranslate(): void {
     this.title = $$("myapp");
   }
   onAppResInit(): void {
-    let self = this;
-    this.onReady = function() {
-      console.log(">>> onReady from component");
-      self.onAppRes();
+    const self = this;
+    this.onReady = function(sender) {
+      self.onAppResTranslate();
     }
-    this.onLanguageChange = function() {
-      console.log(">>> onLanguageChange from component");
-      self.onAppRes();
+    this.onLanguageChange = function(sender) {
+      self.onAppResTranslate();
     }
-    if($appresBinding$["OnReady"]==null) $appresBinding$["OnReady"] = [];
-    if($appresBinding$["OnLanguageChange"]==null) $appresBinding$["OnLanguageChange"] = [];
-    $appresBinding$["OnReady"].push(this.onReady);
-    $appresBinding$["OnLanguageChange"].push(this.onLanguageChange);
+    $$().addEvent("onReady", this.onReady);
+    $$().addEvent("onLanguageChange", this.onLanguageChange);
   }
   onAppResDestroy(): void {
-    let index = $appresBinding$["OnReady"].indexOf(this.onReady);
-    if(index>=0) $appresBinding$["OnReady"].splice(index, 1);      
-    index = $appresBinding$["OnLanguageChange"].indexOf(this.onLanguageChange);
-    if(index>=0) $appresBinding$["OnLanguageChange"].splice(index, 1);      
+    $$().removeEvent("onReady", this.onReady);
+    $$().removeEvent("onLanguageChange", this.onLanguageChange);
   }
 
   ngOnInit(): void {
